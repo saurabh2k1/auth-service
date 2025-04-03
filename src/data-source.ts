9c5327999c6c43b44ebeeedfc71ba2007fba51c7
Saurabh Sharma <saurabh@petronetlng.in>
2025-04-03 11:20:59 +0530

    DB Host change

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres", 
-  host: "localhost",
+  host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
